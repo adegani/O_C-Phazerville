1f0fd1b461c45c6ad4c7943c22a407e376f14b44
Alessio Degani <alessio.degani@gmail.com>
2025-11-30 14:15:53 +0100

    EnsOscKey optimization: recalc output voltage only when needed, elemination of CH2

--- software/src/applets/EnsOscKey.h
+++ software/src/applets/EnsOscKey.h
@@ -29,7 +29,7 @@
 // mixolydian: I(0), ii(2), iii(dim)(4), IV(5), v(7), vi(9), VII(10)
 // aeolian (minor):  i(0), ii(dim)(2), III(3), iv(5), v(7), VI(8), VII(10)
 
-
+#define ONE_VOLT HSAPPLICATION_5V / 5
 class EnsOscKey : public HemisphereApplet {
 public:
 
@@ -39,206 +39,176 @@ public:
 
     void Start() {
         cursor = 0;
-        scale[0] = 6; // Ionian
-        QuantizerConfigure(0, scale[0]);
-        last_note[0] = 0;
-        continuous[0] = 1;
+        scale = 6; // Ionian
+        QuantizerConfigure(0, scale);
+        last_note = 0;
+        continuous = 1;
+        code_maj = voltageToCode(voltage_maj);
+        code_min = voltageToCode(voltage_min);
+        code_dim = voltageToCode(voltage_dim);
+        code_no_match = voltageToCode(voltage_no_match);
+    }
+
+    int voltageToCode(int voltageIn) {
+        return (int)(((float)voltageIn / 2.0 - 0.25) * ONE_VOLT);
     }
 
     int determineInterval(int scale, int rootNote, int currentNote) {
-      // TODO: Teensy 3.2 chokes on all these floats when loaded on both sides!
-
-        int one_volt = HSAPPLICATION_5V / 5;
-        double maj_temp = (voltage_maj / 2.0 - 0.25) * one_volt;
-        int maj_out = static_cast<int>(maj_temp);
-        double min_temp = (voltage_min / 2.0 - 0.25) * one_volt;
-        int min_out = static_cast<int>(min_temp);
-        double dim_temp = (voltage_dim / 2.0 - 0.25) * one_volt;
-        int dim_out = static_cast<int>(dim_temp);
-        double no_match_temp = (voltage_no_match / 2.0 - 0.25) * one_volt;
-        int no_match_out = static_cast<int>(no_match_temp);
-    // Calculate the number of semitones between root and current note
+      
+        // Calculate the number of semitones between root and current note
         int interval = (currentNote - rootNote + 12) % 12;
-
-    // Determine the interval based on the scale
+        int voltageToUse = code_no_match;
+        // Determine the interval based on the scale
         switch (scale) {
+            case 5: // Semi
+                chord_quality = 0;
+                voltageToUse = code_maj;
             case 6:  // Ionian (major)
                 if (interval == 0 || interval == 5 || interval == 7) // Major interval
                     {
                     chord_quality = 0;
-                    return maj_out;
+                    voltageToUse = code_maj;
                     }
                 else if (interval == 2 || interval == 4 || interval == 9) // Minor interval
                     {
                     chord_quality = 1;
-                    return min_out;
+                    voltageToUse = code_min;
                     }
                 else if (interval == 11) // Diminished interval
                     {
                     chord_quality = 2;
-                    return dim_out;
+                    voltageToUse = code_dim;
                     }
                 else {
                     chord_quality = 3;
-                    return no_match_out;
+                    voltageToUse = code_no_match;
                 }
                 break;
             case 7: // Dorian
                 if (interval == 3 || interval == 5 || interval == 10) // Major interval
                     {
                     chord_quality = 0;
-                    return maj_out;
+                    voltageToUse = code_maj;
                     }
                 else if (interval == 0 || interval == 2 || interval == 7) // Minor interval
                     {
                     chord_quality = 1;
-                    return min_out;
+                    voltageToUse = code_min;
                     }
                 else if (interval == 9) // Diminished interval
                     {
                     chord_quality = 2;
-                    return dim_out;
+                    voltageToUse = code_dim;
                     }
                 else {
                     chord_quality = 3;
-                    return no_match_out;
+                    voltageToUse = code_no_match;
                     }
                 break;
             case 8: // Phrygian
                 if (interval == 1 || interval == 3 || interval == 8) // Major interval
                     {
                     chord_quality = 0;
-                    return maj_out;
+                    voltageToUse = code_maj;
                     }
                 else if (interval == 0 || interval == 5 || interval == 10) // Minor interval
                    {
                     chord_quality = 1;
-                    return min_out;
+                    voltageToUse = code_min;
                     }
                 else if (interval == 7) // Diminished interval
                     {
                     chord_quality = 2;
-                    return dim_out;
+                    voltageToUse = code_dim;
                     }
                 else {
                     chord_quality = 3;
-                    return no_match_out;
+                    voltageToUse = code_no_match;
                     }
                 break;
             case 9: // Lydian
                 if (interval == 0 || interval == 2 || interval == 7) // Major interval
                     {
                     chord_quality = 0;
-                    return maj_out;
+                    voltageToUse = code_maj;
                     }
                 else if (interval == 4 || interval == 9 || interval == 11) // Minor interval
                     {
                     chord_quality = 1;
-                    return min_out;
+                    voltageToUse = code_min;
                     }
                 else if (interval == 6) // Diminished interval
                     {
                     chord_quality = 2;
-                    return dim_out;
+                    voltageToUse = code_dim;
                     }
                 else {
                     chord_quality = 3;
-                    return no_match_out;
+                    voltageToUse = code_no_match;
                     }
                 break;
             case 10: // Mixolydian
                 if (interval == 0 || interval == 5 || interval == 10) // Major interval
                     {
                     chord_quality = 0;
-                    return maj_out;
+                    voltageToUse = code_maj;
                     }
                 else if (interval == 2 || interval == 7 || interval == 9) // Minor interval
                     {
                     chord_quality = 1;
-                    return min_out;
+                    voltageToUse = code_min;
                     }
                 else if (interval == 4) // Diminished interval
                     {
                     chord_quality = 2;
-                    return dim_out;
+                    voltageToUse = code_dim;
                     }
                 else {
                     chord_quality = 3;
-                    return no_match_out;
+                    voltageToUse = code_no_match;
                     }
                 break;
             case 11: // Aeolian (minor)
                 if (interval == 3 || interval == 8 || interval == 10) // Major interval
                     {
                     chord_quality = 0;
-                    return maj_out;
+                    voltageToUse = code_maj;
                     }
                 else if (interval == 0 || interval == 5 || interval == 7) // Minor interval
                     {
                     chord_quality = 1;
-                    return min_out;
+                    voltageToUse = code_min;
                     }
                 else if (interval == 2) // Diminished interval
                     {
                     chord_quality = 2;
-                    return dim_out;
+                    voltageToUse = code_dim;
                     }
                 else {
                     chord_quality = 3;
-                    return no_match_out;
+                    voltageToUse = code_no_match;
                     }
                 break;
         }
         // Default case
-        return no_match_out;
+        return voltageToUse;
     }
 
     void Controller() {
         if (Clock(0)) {
-            continuous[0] = 0; // Turn off continuous mode if there's a clock
+            continuous = 0; // Turn off continuous mode if there's a clock
             StartADCLag(0);
         }
         
-        if (continuous[0] || EndOfADCLag(0)) {
+        if (continuous || EndOfADCLag(0)) {
             int32_t pitch = In(0);
-            int32_t quantized = Quantize(0, pitch, root[0] << 7, 0);
+            int32_t quantized = Quantize(0, pitch, root << 7, 0);
             int semitone = (quantized / 128) % 12;
-            int output_voltage = determineInterval(scale[0], root[0], semitone);
+            int output_voltage = determineInterval(scale, root, semitone);
             
             Out(0, output_voltage);
             last_note[0] = quantized;
-        }
-
-        // on channel 2, for each clock pulse, move the cursor to the next setting, or if the cursor is !Edit mode, do CursorToggle to set it into Edit mode. Let's make this only cycle through cursor 2-5
-        if (Clock(1)) {
-            if (EditMode()) {
-                if (cursor < 2) {
-                    cursor = 2;
-                } else if (cursor < 5) {
-                    cursor++;
-                } else {
-                    cursor = 2;
-                }
-            } else {
-                CursorToggle();
-                cursor = 2;
-            }
-        }
-
-        // change the voltage_{chord_quality} of the selected setting with the voltage input in channel 2
-        int32_t voltage = In(1);
-        // scale voltage to 0-10
-        int voltage_setting = voltage / 409;
-        if (EditMode() && voltage_setting > 0 && voltage_setting < 11) {
-            if (cursor == 2) {
-                voltage_maj = voltage_setting;
-                    } else if (cursor == 3) {
-                voltage_min = voltage_setting;
-            } else if (cursor == 4) {
-                voltage_dim = voltage_setting;
-            } else if (cursor == 5) {
-                voltage_no_match = voltage_setting;
-            }
+            last_note = quantized;
         }
     }
 
@@ -256,47 +226,52 @@ public:
 
         if (cursor == 0) {
              // Root selection
-            root[0] = constrain(root[0] + direction, 0, 11);
+            root = constrain(root + direction, 0, 11);
         } else if (cursor == 1) {
            // Scale selection
-            scale[0] += direction;
+            scale += direction;
             // only allow Ionian and Aeolian scales
-            scale[0] = constrain(scale[0], 6, 11);
-            QuantizerConfigure(0, scale[0]);
-            continuous[0] = 1; // Re-enable continuous mode when scale is changed
+            scale = constrain(scale, 6, 11);
+            // scale = constrain(scale, 0, OC::Scales::NUM_SCALES - 1);
+            QuantizerConfigure(0, scale);
+            continuous = 1; // Re-enable continuous mode when scale is changed
         } else if (cursor == 2) {
             // change voltage_maj value
             voltage_maj += direction;
             voltage_maj = constrain(voltage_maj, 1, 10);
+            code_maj = voltageToCode(voltage_maj);
         } else if (cursor == 3) {
             voltage_min += direction;
             voltage_min = constrain(voltage_min, 1, 10);
+            code_min = voltageToCode(voltage_min);
         } else if (cursor == 4) {
             voltage_dim += direction;
             voltage_dim = constrain(voltage_dim, 1, 10);
+            code_dim = voltageToCode(voltage_dim);
         } else if (cursor == 5) {
             voltage_no_match += direction;
             voltage_no_match = constrain(voltage_no_match, 1, 10);
+            code_no_match = voltageToCode(voltage_no_match);
         }
     }
 
     uint64_t OnDataRequest() {
         uint64_t data = 0;
-        Pack(data, PackLocation {0,8}, scale[0]);
-        Pack(data, PackLocation {8,8}, scale[1]);
-        Pack(data, PackLocation {16,4}, root[0]);
-        Pack(data, PackLocation {20,4}, root[1]);
+        Pack(data, PackLocation {0,8}, scale);
+        // Pack(data, PackLocation {8,8}, scale[1]);
+        Pack(data, PackLocation {16,4}, root);
+        // Pack(data, PackLocation {20,4}, root[1]);
         return data;
     }
 
     void OnDataReceive(uint64_t data) {
-        scale[0] = Unpack(data, PackLocation {0,8});
-        scale[1] = Unpack(data, PackLocation {8,8});
-        root[0] = Unpack(data, PackLocation {16,4});
-        root[1] = Unpack(data, PackLocation {20,4});
+        scale = Unpack(data, PackLocation {0,8});
+        // scale[1] = Unpack(data, PackLocation {8,8});
+        root = Unpack(data, PackLocation {16,4});
+        // root[1] = Unpack(data, PackLocation {20,4});
 
-        root[0] = constrain(root[0], 0, 11);
-        QuantizerConfigure(0, scale[0]);
+        root = constrain(root, 0, 11);
+        QuantizerConfigure(0, scale);
     }
 
 protected:
@@ -314,18 +289,23 @@ protected:
   }
 
 private:
-    int last_note[2]; // Last quantized note
-    bool continuous[2]; // Each channel starts as continuous and becomes clocked when a clock is received
+    int last_note; // Last quantized note
+    bool continuous; // Each channel starts as continuous and becomes clocked when a clock is received
     int cursor;
 
     // Settings
-    int scale[2]; // Scale per channel
-    uint8_t root[2]; // Root per channel
+    int scale; // Scale 
+    uint8_t root; // Root
 
     int voltage_maj = 3;
     int voltage_min = 4;
     int voltage_dim = 5;
     int voltage_no_match = 6;
+
+    int code_maj = 0;
+    int code_min = 0;
+    int code_dim = 0;
+    int code_no_match = 0;
 
     int chord_quality = 0; // 0 = Maj, 1 = Min, 2 = Dim, 3 = No Match
 
@@ -335,12 +315,12 @@ private:
 
 
         // Draw Key Information
-        gfxPrint(27, 15, OC::scale_names_short[scale[0]]);
+        gfxPrint(27, 15, OC::scale_names_short[scale]);
         gfxBitmap(0, 15, 8, notes[0]);
-        gfxPrint(10, 15, OC::Strings::note_names_unpadded[root[0]]);
+        gfxPrint(10, 15, OC::Strings::note_names_unpadded[root]);
 
         // Draw Resulting Note Information
-        int semitone = ((last_note[0] / 128) % 12 + 12) % 12;
+        int semitone = ((last_note / 128) % 12 + 12) % 12;
         gfxPrint(10, 27, OC::Strings::note_names_unpadded[semitone]);
         gfxPrint(0, 27, "=");
 
